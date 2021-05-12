90e9d2fc95b246dfc019738402603d322b562f05
Katharine Hao <hao.katharine@gmail.com>
2021-05-12 23:44:49 +0300

    did show page for lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ movie['results'].each do |movie|
   Movie.create(
     title: movie['original_title'],
     overview: movie['overview'],
-    poster_url: "https://image.tmdb.org/t/p/original/#{movie['poster_path']}",
+    poster_url: "https://image.tmdb.org/t/p/original/#{movie['backdrop_path']}",
     rating: movie['vote_average']
   )
 end
