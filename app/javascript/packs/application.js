84335ec85e469d4cd42ae9c77484b2bff40051eb
Katharine Hao <hao.katharine@gmail.com>
2021-05-12 20:18:51 +0300

    added some js and css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 
@@ -23,6 +25,10 @@ document.addEventListener('turbolinks:load', () => {
   // [...]
   loadDynamicBannerText();
 });
+
+AOS.init();
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
